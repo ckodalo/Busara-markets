b1717a747a3dac41787aee488e5870e4d71c14cb
23po <ckodalo@gmail.com>
2024-03-27 23:31:56 +0300

    adds home controller and view

---
+++ src/main/java/com/predictions/predictions/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.predictions.predictions.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        // Add any data you want to pass to the view
+        model.addAttribute("pageTitle", "Welcome to Prediction Markets");
+        return "home"; // Return the name of the Thymeleaf template (e.g., home.html)
+    }
+}
