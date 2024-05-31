f6a4f0af215e07a730a7fb578c19d22a646a62ec
23po <ckodalo@gmail.com>
2024-05-31 20:02:09 +0300

    adds security config

--- src/main/java/com/predictions/predictions/controllers/UserController.java
+++ src/main/java/com/predictions/predictions/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.predictions.predictions.models.User;
 import com.predictions.predictions.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +20,30 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/login")
+    private String login () {
+
+        return "fragments/login-modal";
+
+    }
+
 
     @PostMapping("/login")
     private String login (@ModelAttribute User user) {
 
+        if (user == null) {
+
+            throw new RuntimeException("user cant be null");
+        }
+
         try {
+
+            System.out.println("user detils is " + user);
             userService.findUser(user);
         }
         catch (Exception e) {
 
-            throw new RuntimeException();
+            throw new RuntimeException("user not found");
         }
 
         return "redirect:/markets";
