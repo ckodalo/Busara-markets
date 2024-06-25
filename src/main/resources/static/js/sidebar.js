aefb56797c371f3cb7c9fe11652c8b151653bd5e
23po <ckodalo@gmail.com>
2024-06-25 20:17:26 +0300

    adds collapsible panel for smaller screens

---
+++ src/main/resources/static/js/sidebar.js
@@ -0,0 +1,9 @@
+function openPanel() {
+    const aside = document.getElementById('collapsible-panel');
+    aside.style.width = "250px";
+}
+
+function closePanel() {
+  document.getElementById("collapsible-panel").style.width = "0";
+}
+
